90fe7abbc060f16cd4aa42e7e929eb1a5821dab2
quaker66 <quaker66@gmail.com>
2011-01-30 19:54:24 +0100

    Commit machine continues pushing. More changes. WIP.

--- src/cubecreate/scripting_system_lua_def.hpp
+++ src/cubecreate/scripting_system_lua_def.hpp
@@ -1717,6 +1717,129 @@ LUA_BIND_CLIENT(jump, {
     }
 })
 
+// intensity/client_system.cpp
+
+LUA_BIND_CLIENT(connect_to_instance, {
+    REFLECT_PYTHON(login_to_instance);
+    login_to_instance(e.get<const char*>(1));
+})
+
+LUA_BIND_CLIENT(connect_to_lobby, {
+    REFLECT_PYTHON(connect_to_lobby);
+    connect_to_lobby();
+})
+
+LUA_BIND_CLIENT(connect_to_selected_instance, {
+    REFLECT_PYTHON(connect_to_selected_instance);
+    connect_to_selected_instance();
+})
+
+LUA_BIND_CLIENT(show_instances, {
+    REFLECT_PYTHON(get_possible_instances);
+    boost::python::object instances = get_possible_instances();
+    REFLECT_PYTHON(None);
+
+    if (instances == None)
+    {
+        SETVF(error_message, "Could not get the list of instances");
+        showgui("error");
+        return;
+    }
+
+    char buf[1024];
+    snprintf(buf, sizeof(buf),
+        "GUI.new(\"instances\", [[\n"
+        "    GUI.text(\"Pick an instance to enter:\")\n"
+        "    GUI.bar()\n");
+    char *command = (char*)malloc(strlen(buf) + 1);
+    strcpy(command, buf);
+
+    int ninst = boost::python::extract<int>(instances.attr("__len__")());
+    for (int i = 0; i < ninst; i++)
+    {
+        boost::python::object instance = instances[i];
+        const char *instance_id = boost::python::extract<const char*>(instance.attr("__getitem__")("instance_id"));
+        const char *event_name = boost::python::extract<const char*>(instance.attr("__getitem__")("event_name"));
+
+        assert( Utility::validateAlphaNumeric(instance_id) );
+        assert( Utility::validateAlphaNumeric(event_name, " (),.;") ); // XXX: Allow more than alphanumeric+spaces: ()s, .s, etc.
+
+        snprintf(buf, sizeof(buf), "    GUI.button(\"%s\", \"CE.connect_to_instance(%s)\")\n", event_name, instance_id);
+        command = (char*)realloc(command, strlen(command) + strlen(buf) + 1);
+        assert(command);
+        strcat(command, buf);
+    }
+
+    snprintf(buf, sizeof(buf), "]])\nGUI.show(\"instances\")\n");
+    command = (char*)realloc(command, strlen(command) + strlen(buf) + 1);
+    assert(command);
+    strcat(command, buf);
+
+    Logging::log(Logging::DEBUG, "Instances GUI: %s\r\n", command);
+    engine.exec(command);
+})
+
+LUA_BIND_CLIENT(do_upload, {
+    renderprogress(0.1, "compiling scripts ..");
+
+    REFLECT_PYTHON(get_map_script_filename);
+    const char *fname = boost::python::extract<const char*>(get_map_script_filename());
+    if (!engine.load(Utility::readFile(fname).c_str()))
+    {
+        IntensityGUI::showMessage("Compilation failed", engine.geterror_last());
+        return;
+    }
+
+    renderprogress(0.3, "generating map ..");
+    save_world(game::getclientmap());
+
+    renderprogress(0.4, "exporting entities ..");
+    REFLECT_PYTHON(export_entities);
+    export_entities("entities.json");
+
+    renderprogress(0.5, "uploading map ..");
+    REFLECT_PYTHON(upload_map);
+    upload_map();
+
+    REFLECT_PYTHON(get_curr_map_asset_id);
+    const char *aid = boost::python::extract<const char*>(get_curr_map_asset_id());
+    SETVF(last_uploaded_map_asset, aid);
+})
+
+/* Reuploads asset - doesn't save world and doesn't require one running - useful
+ * for reuploading stuff crashing etc
+ */
+LUA_BIND_CLIENT(repeat_upload, {
+    const char *lumasset = GETSV(last_uploaded_map_asset);
+
+    renderprogress(0.2, "getting map asset info ..");
+    REFLECT_PYTHON(AssetManager);
+    boost::python::object ainfo = AssetManager.attr("get_info")(lumasset);
+
+    REFLECT_PYTHON(set_curr_map_asset_id);
+    set_curr_map_asset_id(lumasset);
+
+    REFLECT_PYTHON(World);
+    World.attr("asset_info") = ainfo;
+
+    renderprogress(0.5, "compiling scripts ..");
+    REFLECT_PYTHON(get_map_script_filename);
+    const char *fname = boost::python::extract<const char*>(get_map_script_filename());
+    if (!engine.load(Utility::readFile(fname).c_str()))
+    {
+        IntensityGUI::showMessage("Compilation failed", engine.geterror_last());
+        return;
+    }
+
+    renderprogress(0.7, "uploading map ..");
+    REFLECT_PYTHON(upload_map);
+    upload_map();
+
+    conoutf("Upload complete.");
+})
+
+LUA_BIND_STD_CLIENT(restart_map, MessageSystem::send_RestartMap)
+
 // intensity/editing_system.cpp
 
 LUA_BIND_CLIENT(save_mouse_pos, {
